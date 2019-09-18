be289bc024307ce8ae612e6badb1472aea007113
Ilias Bertsimas <ilias.bertsimas@sainsburys.co.uk>
2019-09-18 13:32:35 +0100

    Destroy prompt improvements.

--- tf_helper/destroy.go
+++ tf_helper/destroy.go
@@ -8,13 +8,23 @@ import (
 	"strings"
 )
 
+const CLR_0 = "\x1b[30;1m"
+const CLR_R = "\x1b[31;1m"
+const CLR_G = "\x1b[32;1m"
+const CLR_Y = "\x1b[33;1m"
+const CLR_B = "\x1b[34;1m"
+const CLR_M = "\x1b[35;1m"
+const CLR_C = "\x1b[36;1m"
+const CLR_W = "\x1b[37;1m"
+const CLR_N = "\x1b[0m"
+
 func (c *Config) Destroy(parallelism int16) {
 
 	//We want to really ensure destroy is what you want to do, therefore we double check here, requiring an input from CLI
 
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("You are running Destroy! Are you sure you want to proceed? Confirm by entering Destroy: ")
+	fmt.Printf("%sYou are running Destroy! Are you sure you want to proceed? Confirm by entering \"destroy\": ", CLR_R)
 
 	input, _ := reader.ReadString('\n')
 
